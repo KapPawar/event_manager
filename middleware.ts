9ade9ebe331bea35fc566edc851b51097bcc7238
KapPawar <pawar.kapil.shyam@gmail.com>
2023-12-28 09:32:43 -0600

    first commit

---
+++ middleware.ts
@@ -0,0 +1,20 @@
+import { authMiddleware } from "@clerk/nextjs";
+
+export default authMiddleware({
+  publicRoutes: [
+    "/",
+    "/events/:id",
+    "/api/webhook/clerk",
+    "/api/webhook/strip",
+    "/api/uploadthing",
+  ],
+  ignoredRoutes: [
+    "/api/webhook/clerk",
+    "/api/webhook/strip",
+    "/api/uploadthing",
+  ],
+});
+
+export const config = {
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+};
