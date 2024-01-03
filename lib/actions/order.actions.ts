44c2e8fa566e27a7c4567fc917a5c8e8abdf8e96
KapPawar <pawar.kapil.shyam@gmail.com>
2024-01-02 23:49:44 -0600

    implement stripe

--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -42,11 +42,13 @@ export const checkoutOrder = async (order: CheckoutOrderParams) => {
 export const createOrder = async (order: CreateOrderParams) => {
   try {
     await connectToDatabase();
+
     const newOrder = await Order.create({
       ...order,
       event: order.eventId,
       buyer: order.buyerId,
     });
+
     return JSON.parse(JSON.stringify(newOrder));
   } catch (error) {
     handleError(error);
